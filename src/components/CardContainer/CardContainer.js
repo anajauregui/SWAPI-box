cd534e296f6e9da905e5f6730484e57a6d772ce8
Ana <analujauregui@gmail.com>
2017-08-23 14:40:27 -0600

    Appends appropriate cards on button click

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 import Cards from '../Cards/Cards';
+import MovieScroll from '../MovieScroll/MovieScroll';
 import './CardContainer.css';
 
-const CardContainer = ({ peopleArray }) => {
-	let cardArray = peopleArray.map((el, index) => {
-		return <Cards key={index} name={el.name} homeworld={el.homeworld} species={el.species} population={el.population}/>
-	}
-	);
+const CardContainer = ({ peopleArray, planetArray, vehicleArray, display, scrollData }) => {
+
+	const peopleCardArray = peopleArray.map((el, index) => {
+		return <Cards key={index} name={el.name} homeworld={`Homeworld: ${el.homeworld}`} species={`Species: ${el.species}`} population={`Population: ${el.population}`}/>
+	});
+	const planetCardArray = planetArray.map((el, index) => {
+		// const residents = el.residents.map(res => res)
+		return <Cards key={index} name={el.name} terrain={`Terrain: ${el.terrain}`} population={`Population: ${el.population}`} climate={`Climate: ${el.climate}`} residents={`Famous Residents: ${el.residents}`} />
+	});
+	const vehicleCardArray = vehicleArray.map((el, index) => {
+		return <Cards key={index} name={el.name} vehicleClass={`Class: ${el.vehicle_class}`} model={`Model: ${el.model}`} passengers={`# Passengers: ${el.passengers}`} />
+	});
 
 	return (
 		<section className="card-container">
-			{cardArray}
+			{ (display === 'People') ? peopleCardArray : null }
+			{ (display === 'Planets') ? planetCardArray : null }
+			{ (display === 'Vehicles') ? vehicleCardArray : null }
+			{ display === 'welcome' && <MovieScroll scrollData={scrollData} /> }
 		</section>
 	);
 };
